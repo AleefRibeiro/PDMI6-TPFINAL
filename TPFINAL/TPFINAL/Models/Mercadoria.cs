6a2ddb18084397b4ee1ebca05a0248f24e283148
AleefRibeiro <alef129@gmail.com>
2022-01-15 15:37:53 -0300

    Fix some bugs and edit identation

--- TPFINAL/TPFINAL/Models/Mercadoria.cs
+++ TPFINAL/TPFINAL/Models/Mercadoria.cs
@@ -6,16 +6,15 @@ using System.Text;
 using Xamarin.Forms;
 using TPFINAL.Data;
 
-namespace TPFINAL.Models
-{
-    public class Mercadoria
-    {
+namespace TPFINAL.Models {
+    public class Mercadoria {
         private SQLiteConnection database;
-        public Mercadoria()
-        {
+
+        public Mercadoria() {
             database = DependencyService.Get<ISQLite>().GetConexao();
             database.CreateTable<Mercadoria>();
         }
+        
         public string Nome { get; set; }
         public string Peso { get; set; }
         public string NomeProdutor { get; set; }
@@ -26,37 +25,31 @@ namespace TPFINAL.Models
 
         static object locker = new object();
 
-        public int SalvarMercadoria(Mercadoria mercadoria)
-        {
-            lock (locker)
-            {
-                if (GetMercadoria(mercadoria.NCM) != null)
-                {
+        public int SalvarMercadoria(Mercadoria mercadoria) {
+            lock (locker) {
+                if (GetMercadoria(mercadoria.NCM) != null) {
                     return database.Update(mercadoria);
                 }
                 return database.Insert(mercadoria);
             }
         }
 
-        public IEnumerable<Mercadoria> GetMercadorias()
-        {
-            lock (locker)
-            {
+        public IEnumerable<Mercadoria> GetMercadorias() {
+            lock (locker) {
                 return (from c in database.Table<Mercadoria>()
                         select c).ToList();
             }
         }
-        public Mercadoria GetMercadoria(int NCM)
-        {
+
+        public Mercadoria GetMercadoria(int NCM) {
             lock (locker)
             {
                 return database.Table<Mercadoria>().Where(c => c.NCM == NCM).FirstOrDefault();
             }
         }
-        public int RemoverMercadoria(int NCM)
-        {
-            lock (locker)
-            {
+
+        public int RemoverMercadoria(int NCM) {
+            lock (locker) {
                 return database.Delete<Mercadoria>(NCM);
             }
         }
